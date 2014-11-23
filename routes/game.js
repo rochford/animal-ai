2475890f47b2dbd7832a95a3ce02e2df19d7f153
timothy.rochford <timothy.rochford@b1ad7931-ab00-b378-c749-0800159d46a1>
2014-11-23 18:40:45 +0000

    GZIP compression added. 'use strict' enabled.

--- routes/game.js
+++ routes/game.js
@@ -16,6 +16,8 @@
     You should have received a copy of the GNU Lesser General Public License
     along with Animal AI.  If not, see <http://www.gnu.org/licenses/>.
 */
+"use strict"
+
 var _ = require('underscore');
 var utils = require('./utils.js');
 var mongo = require('./mongo.js');

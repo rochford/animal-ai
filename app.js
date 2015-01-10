489540ee862c7ab2cae693673a7a61165dcd07b1
timothy.rochford <timothy.rochford@b1ad7931-ab00-b378-c749-0800159d46a1>
2015-01-10 19:46:02 +0000

    Updated to Express 4.0

--- app.js
+++ app.js
@@ -19,64 +19,70 @@
 "use strict";
 
 var express = require('express'),
-    index = require('./routes/index.js'),
-    game = require('./routes/game.js'),
-    guess = require('./routes/guess.js'),
-    addanimal = require('./routes/addanimal.js'),
-    http = require('http'),
-    path = require('path'),
-    _ = require('underscore'),
-    utils = require('./routes/utils.js'),
-    mongo = require('./routes/mongo.js'),
-    MongoStore = require('connect-mongo')(express);
-
+        addanimal = require('./routes/addanimal.js'),
+        bodyParser = require('body-parser'),
+        compression = require('compression'),
+        connect = require('connect'),
+        cookieParser = require('cookie-parser'),
+        errorhandler = require('errorhandler'),
+        expresssession = require('express-session'),
+        game = require('./routes/game.js'),
+        guess = require('./routes/guess.js'),
+        http = require('http'),
+        index = require('./routes/index.js'),
+        methodOverride = require('method-override'),
+        morgan = require('morgan'),
+        mongo = require('./routes/mongo.js'),
+        session = require('express-session'),
+        mongoStore = require('connect-mongo')(session),
+        path = require('path'),
+        utils = require('./routes/utils.js'),
+        _ = require('underscore');
 var app = express();
 
-app.configure(function() {
-    app.set('port', process.env.PORT || 3000);
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'jade');
-    app.use(function (req, res, next) {
-        if ('/robots.txt' === req.url) {
-            res.type('text/plain');
-            res.send("User-agent: *\nAllow: /\nDisallow: /css/\nDisallow: /lib/\nDisallow: /bootstrap/\nDisallow: /game\nDisallow: /animal\nDisallow: /question\nDisallow: /guess\nDisallow: /animal_added");
-        } else {
-            next();
-        }
-    });
-
-    app.use(express.compress());
-    app.use(express.cookieParser()); // cookie secret is session secret
-    app.use(express.session({
-        secret: process.env.COOKIE_SECRET || 'aij',
-        cookie: { maxAge: new Date(Date.now() + 3600000*24*7*2) }, // 2 weeks
-        store: new MongoStore({ url: process.env.MONGO_SERVER_URL,
-                                collection: 'sessions'
-        })
-      }));
-    app.use(express.urlencoded()); // to support URL-encoded bodies
-    app.use(express.favicon());
-    app.use(express.logger('dev'));
-    app.use(function (req, res, next) {
-        var ip = utils.getClientIp(req);
-
-        if (ip === process.env.DEV_IP) {
-            req.session.analytics = false;
-        } else {
-            req.session.analytics = true;
-        }
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(function (req, res, next) {
+    if ('/robots.txt' === req.url) {
+        res.type('text/plain');
+        res.send("User-agent: *\nAllow: /\nDisallow: /css/\nDisallow: /lib/\nDisallow: /bootstrap/\nDisallow: /game\nDisallow: /animal\nDisallow: /question\nDisallow: /guess\nDisallow: /animal_added");
+    } else {
         next();
-    });
-
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(app.router);
-    app.use(express.static(path.join(__dirname, 'public')));
+    }
 });
 
-app.configure('development', function () {
-    app.use(express.errorHandler());
+app.use(compression({threshold: 512}));
+app.use(cookieParser()); // cookie secret is session secret
+app.use(session({
+                    secret: process.env.COOKIE_SECRET || 'aij',
+                    resave: false,
+                    saveUninitialized: true,
+                    cookie: { maxAge: new Date(Date.now() + 3600000*24*7*2) }, // 2 weeks
+                    store: new mongoStore({ url: process.env.MONGO_SERVER_URL,
+                                              collection: 'sessions'
+                                          })
+                }));
+app.use(bodyParser.urlencoded({ extended: false })); // to support URL-encoded bodies
+app.use(morgan('dev'));
+app.use(function (req, res, next) {
+    var ip = utils.getClientIp(req);
+
+    if (ip === process.env.DEV_IP) {
+        req.session.analytics = false;
+    } else {
+        req.session.analytics = true;
+    }
+    next();
 });
+
+app.use(methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+
+var env = process.env.NODE_ENV || 'development';
+if ('development' == env) {
+    app.use(errorhandler());
+}
 
 app.get('/', index.index);
 app.get('/about', index.about);
